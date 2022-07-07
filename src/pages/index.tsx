6378b14bca3ea28b7c11e2aed2bfd28e0b024d7b
Cory Sougstad <cory@hau.me>
2022-07-07 15:31:51 -0700

    Replace headline with logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import logo from "../../public/teaser-logo.jpg";
 //import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
@@ -14,9 +16,7 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="w-screen min-h-screen flex flex-col justify-center items-center p-4 overflow-y-scroll">
-        <h2 className="text-[3rem] lg:text-[5rem] md:text-[5rem] font-extrabold text-gray-700">
-          2 Lies n a Crime Podcast
-        </h2>
+        <Image src={logo} height={400} width={400} alt="Illistration of two cats sitting on a crecent moon, holding wine glasses with their tails, with text 2 lies in a crime." />
         <p>Coming soon...</p>
       </div>
     </>
