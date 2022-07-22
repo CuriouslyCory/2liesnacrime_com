a497277dae55286f7c4b2548153817372bd0fa31
Cory Sougstad <cory@hau.me>
2022-07-22 14:17:06 -0700

    Add episode details page and social meta data

---
+++ src/pages/episode/[slug].tsx
@@ -0,0 +1,44 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { trpc } from "../../utils/trpc";
+import logo from "../../../public/full-logo.svg";
+import { EpisodeCard } from "../../components/episode-card";
+
+export const EpisodePage: NextPage = (): JSX.Element => {
+    const router = useRouter();
+    const { slug } = router.query;
+    const { data: episode, error, status } = trpc.useQuery(
+        [
+            "episode.bySlug",
+            { slug : !!slug && !Array.isArray(slug) ? slug : "" }
+        ], 
+        { enabled: !!slug }
+    );
+
+    return (
+    <>
+        <Head>
+            {!!episode && (
+                [
+                    <meta property="og:locale" content="en_US" />,
+                    <meta property="og:type" content="article" />,
+                    <meta property="og:title" content={episode.title} />,
+                    <meta property="og:description" content={episode.description} />,
+                    <meta property="og:url" content={`https://www.2liesinacrime.com/episode/${episode.slug}`} />,
+                    <meta property="og:image" content={logo} />,
+                    <meta name="twitter:title" content={episode.title} />,
+                    <meta name="twitter:description" content={episode.description} />,
+                    <meta name="twitter:image" content={logo} />,
+                    <meta name="twitter:card" content="summary_large_image" />,
+                ]
+            )}
+        </Head>
+        {!episode && <span>Loading...</span>}
+        {episode && <EpisodeCard episode={episode} />}
+    </>
+    );
+
+};
+
+export default EpisodePage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
-import Image from "next/image";
-import Link from "next/link";
+import Head from "next/head";
 import logo from "../../public/full-logo.svg";
 import { EpisodeCard } from "../components/episode-card";
 import { trpc } from "../utils/trpc";
@@ -10,10 +9,21 @@ const Home: NextPage = () => {
 
   return (
     <>
+      <Head>
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="article" />,
+        <meta property="og:title" content="Two Lies in a Crime Podcast" />,
+        <meta property="og:description" content="True crime with a twist, some of the story isn't true." />,
+        <meta property="og:url" content="https://www.2liesinacrime.com/" />,
+        <meta property="og:image" content={logo} />,
+        <meta name="twitter:title" content="Two Lies in a Crime Podcast" />,
+        <meta name="twitter:description" content="True crime with a twist, some of the story isn't true." />,
+        <meta name="twitter:image" content={logo} />,
+        <meta name="twitter:card" content="summary_large_image" />,
+      </Head>
       <div className="bg-amber-300 w-full flex justify-center">
         {episodes && episodes.map((episode) => <EpisodeCard key={`episode-card-${episode.id}`} episode={episode} />)}
       </div>
-      
     </>
   );
 };
