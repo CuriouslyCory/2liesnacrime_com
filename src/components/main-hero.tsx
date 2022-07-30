c98081c0d7dbfb791d62259e5e4a990d64f830b1
Cory Sougstad <cory@hau.me>
2022-07-30 12:58:18 -0700

    Add images update layout

---
+++ src/components/main-hero.tsx
@@ -0,0 +1,10 @@
+export const MainHero = (): JSX.Element => {
+  return (
+    <div
+      className="h-96 bg-bottom bg-no-repeat bg-cover"
+      style={{ backgroundImage: "url('/images/wine-cheers.jpg')" }}
+    ></div>
+  );
+};
+
+export default MainHero;
