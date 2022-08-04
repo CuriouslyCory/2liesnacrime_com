6bf40a2fedc82e30d5bbff029c35d00ce35a213e
Cory Sougstad <cory@hau.me>
2022-08-04 16:15:22 -0700

    Add buzzsprout player

--- src/components/episode-card.tsx
+++ src/components/episode-card.tsx
@@ -3,12 +3,20 @@ import Link from "next/link";
 import { FaSpotify } from "react-icons/fa";
 import { InferQueryOutput } from "../utils/trpc";
 import logo from "../../public/full-logo.svg";
+import Script from "next/script";
 
 export type EpisodeCardProps = {
   episode: InferQueryOutput<"episode.byId">;
 };
 
 export const EpisodeCard = ({ episode }: EpisodeCardProps): JSX.Element => {
+  const urlCode = (): string => {
+    const urlPart = episode.audioUrl.split("/");
+    const lastElement = urlPart.pop();
+    const slug = lastElement?.slice(0, -4);
+    return slug || "";
+  };
+
   return (
     <div className="hero flex flex-col sm:flex-col md:flex-row gap-y-5 w-full max-w-6xl mt-20 mb-10 gap-x-5 justify-start">
       <div id="left-section" className="flex flex-col h-full grow">
@@ -23,21 +31,19 @@ export const EpisodeCard = ({ episode }: EpisodeCardProps): JSX.Element => {
         </p>
 
         <div className="block mt-5">
-          <Link href={episode.spotifyUrl}>
-            <button className="block rounded-full p-2 bg-slate-50 flex items-center gap-x-2">
-              <FaSpotify className="text-[#1db954]" />
-              Listen now on Spotify
-            </button>
-          </Link>
+          <div id={`buzzsprout-player-${episode.buzzsproutId}`}></div>
+          <Script
+            src={`https://www.buzzsprout.com/2020224/${urlCode()}.js?container_id=buzzsprout-player-${episode.buzzsproutId}&player=small`}
+            type="text/javascript"
+          />
         </div>
-
         <span className="justify-self-end mt-auto underline">
           <Link href={`/transcripts/${episode.slug}`}>Episode Transcript</Link>
         </span>
       </div>
       <div className="bg-gray-50 p-10 rounded-lg grow justify-center flex">
         <Image
-          src={logo}
+          src={episode.artworkUrl}
           height={250}
           width={250}
           objectFit="contain"
