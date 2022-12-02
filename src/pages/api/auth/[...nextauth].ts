9c5968a3074b5b36d807ac88a2965c967e26a755
Cory Sougstad <cory@hau.me>
2022-12-02 12:03:04 -0700

    manually add secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,5 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -21,6 +20,7 @@ export const authOptions: NextAuthOptions = {
       return Promise.resolve(session);
     },
   },
+  secret: process.env.NEXTAUTH_SECRET ?? "",
 };
 
 export default NextAuth(authOptions);
