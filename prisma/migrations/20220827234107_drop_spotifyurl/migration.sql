301457b08e4dd21baf93ffe085b5424bc71af899
Cory Sougstad <cory@hau.me>
2022-12-02 11:25:41 -0700

    Migrate to supabase

--- prisma/migrations/20220827234107_drop_spotifyurl/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `spotifyUrl` on the `Episode` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Episode" DROP COLUMN "spotifyUrl";
--- prisma/migrations/20221202181736_supa_migration/migration.sql
+++ prisma/migrations/20221202181736_supa_migration/migration.sql
@@ -40,6 +40,7 @@ CREATE TABLE "User" (
     "email" TEXT,
     "emailVerified" TIMESTAMP(3),
     "image" TEXT,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -49,6 +50,51 @@ CREATE TABLE "VerificationToken" (
     "identifier" TEXT NOT NULL,
     "token" TEXT NOT NULL,
     "expires" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Episode" (
+    "id" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "seasonNumber" INTEGER NOT NULL DEFAULT 0,
+    "episodeNumber" INTEGER NOT NULL DEFAULT 0,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "audioUrl" TEXT NOT NULL DEFAULT '',
+    "artworkUrl" TEXT NOT NULL DEFAULT '',
+    "duration" INTEGER NOT NULL DEFAULT 0,
+    "buzzsproutId" INTEGER NOT NULL DEFAULT 0,
+    "buzzsproutGuid" TEXT NOT NULL DEFAULT '',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Episode_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TagsOnEpisodes" (
+    "episodeId" TEXT NOT NULL,
+    "tagId" INTEGER NOT NULL,
+
+    CONSTRAINT "TagsOnEpisodes_pkey" PRIMARY KEY ("episodeId","tagId")
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "episodeId" TEXT NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -66,8 +112,20 @@ CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token"
 -- CreateIndex
 CREATE UNIQUE INDEX "VerificationToken_identifier_token_key" ON "VerificationToken"("identifier", "token");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Episode_slug_key" ON "Episode"("slug");
+
 -- AddForeignKey
 ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TagsOnEpisodes" ADD CONSTRAINT "TagsOnEpisodes_episodeId_fkey" FOREIGN KEY ("episodeId") REFERENCES "Episode"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TagsOnEpisodes" ADD CONSTRAINT "TagsOnEpisodes_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_episodeId_fkey" FOREIGN KEY ("episodeId") REFERENCES "Episode"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
